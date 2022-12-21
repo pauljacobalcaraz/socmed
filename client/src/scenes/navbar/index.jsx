bb3fcd22423f2dcaee9cb2477119c95b48c0253a
Xypher-Alcaraz <pauljacobalcaraz@gmail.com>
2022-12-21 17:09:36 +0800

    save

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -18,9 +18,40 @@ import { useNavigate } from 'react-router-dom';
 import FlexBetween from 'components/FlexBetween';
 
 const NavBar = () => {
+	const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const user = useSelector((state) => state.user);
+	const isNonMobileScreen = useMediaQuery('(mid-width: 1000px)');
+
+	const theme = useTheme();
+	const neutralLight = theme.palette.neutral.light;
+	const dark = theme.palette.neutral.dark;
+	const background = theme.palette.background.default;
+	const primaryLight = theme.palette.primary.light;
+	const alt = theme.palette.background.alt;
+
+	const fulleName = `${user.firstName} ${user.lastName}`;
 	return (
 		<>
-			<div>NavBar</div>
+			<FlexBetween padding='1rem 6%' backgroundColor={alt}>
+				<FlexBetween gap='1.75rem'>
+					<Typography
+						fontWeight='bold'
+						fontSize='clamp(1rem, 2rem,2.25rem )'
+						color='primary'
+						onClick={() => navigate('/home')}
+						sx={{
+							'&:hover': {
+								color: primaryLight,
+								cursor: 'pointer',
+							},
+						}}
+					>
+						Sociopedia
+					</Typography>
+				</FlexBetween>
+			</FlexBetween>
 		</>
 	);
 };
